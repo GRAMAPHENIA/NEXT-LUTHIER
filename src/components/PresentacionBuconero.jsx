3ffe8811872bb53b53f03cea57d95b4f0e97aeea
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-06 11:06:22 -0300

    mods-css

--- src/components/PresentacionBuconero.jsx
+++ src/components/PresentacionBuconero.jsx
@@ -4,8 +4,8 @@ import Logo from "../../public/info.svg";
 export default function Presentacion() {
   return (
     <>
-      <h1 className="ml-9">MODELO BUCONERO NATIVA</h1>
-      <p className="text-3xl font-light font-sans text-white sm:text-2xl lg:text-3xl p-10">
+      <h1 className="ml-5 lg:ml-9 text-5xl">MODELO BUCONERO NATIVA</h1>
+      <p className="text-3xl font-light font-sans text-white sm:text-2xl lg:text-3xl p-6 lg:p-10">
         A partir de la superstrat, conservando todas sus características, con un
         tacto moderno y una amplia variedad de configuraciones.
         <br /> <br />
