7a3c871b892222b7a65d88ee7d2f549585972280
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-08 10:01:00 -0300

    responsive

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -28,15 +28,15 @@ export default function Galeria() {
       </section>
 
       <section className=" gap-2 grid grid-rows-3 grid-cols-3 lg:grid-cols-4 ">
-        <figure className="p-4 row-start-1 row-end-6 col-start-1 col-end-2 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 bg-[#14181b] rounded-2xl">
+        <figure className="h-[260px] p-4 row-start-1 row-end-6 col-start-1 col-end-2 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 bg-[#14181b] rounded-2xl">
           A
         </figure>
 
-        <figure className="p-4 row-start-1 row-end-6 col-start-2 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-4 lg:col-end-6 bg-[#14181b] rounded-2xl">
+        <figure className="h-[260px] p-4 row-start-1 row-end-6 col-start-2 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-4 lg:col-end-6 bg-[#14181b] rounded-2xl">
           B
         </figure>
 
-        <figure className="p-4 row-start-1 row-end-6 col-start-3 col-end-4 lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-4 bg-[#14181b] rounded-2xl">
+        <figure className="h-[260px] p-4 row-start-1 row-end-6 col-start-3 col-end-4 lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-4 bg-[#14181b] rounded-2xl">
           C
         </figure>
       </section>
