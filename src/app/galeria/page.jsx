486577d977e320aeed247c3d4d7cd04b0f63d13e
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-08 11:20:57 -0300

    nav-res

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -8,7 +8,7 @@ export default function Galeria() {
         <figure className=" p-4 row-start-1 row-end-3 col-start-1 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 bg-[#14181b] rounded-2xl">
           <Link
             href="/"
-            className="absolute top-8 left-7 lg:left-28 text-xl text-center w-[55px] px-4 py-3 border-2 border-[#393c40]  hover:bg-[#20262b] rounded-full"
+            className="absolute top-5 left-5 lg:left-28 text-xl text-center w-[55px] px-4 py-3 border-2 border-[#393c40]  hover:bg-[#20262b] rounded-full"
           >
             ↩
           </Link>
