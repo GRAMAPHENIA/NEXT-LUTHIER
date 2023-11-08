859e02aaaa0fcc4c40fb4e76c3c465d2e49b3380
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-08 11:09:05 -0300

    fix-nav

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Galeria() {
   return (
-    <section className="mx-2 lg:mx-10 my-2">
+    <section className="px-4 lg:px-24 py-5">
       <article className=" mb-2 gap-2 grid grid-rows-6 grid-cols-2 lg:mx-15 lg:grid-cols-4 ">
-        <figure className="p-4 row-start-1 row-end-3 col-start-1 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 bg-[#14181b] rounded-2xl">
-          1
+        <figure className=" p-4 row-start-1 row-end-3 col-start-1 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 bg-[#14181b] rounded-2xl">
+          <Link
+            href="/"
+            className="absolute top-8 left-7 lg:left-28 text-xl text-center w-[55px] px-4 py-3 border-2 border-[#393c40]  hover:bg-[#20262b] rounded-full"
+          >
+            ↩
+          </Link>
         </figure>
 
         <figure className="p-4 lg:col-start-2 lg:col-end-3 bg-[#14181b] rounded-2xl">
